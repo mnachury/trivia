fd57e05fb89b33cb90b6c3c798bc10dcbc1d871d
NACHURY MATHIEU p1615457 <mathieuNachury@outlook.com>
2017-04-13 17:33:08 +0200

    Encapsulation et separation des Questions

---
+++ C#/Trivia/Trivia/Questions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trivia
+{
+    class Questions
+    {
+        private List<QuestionStack> _questionStack
+
+        public Questions(Int32[] stackIs)
+        {
+            foreach( int stackId in stackIds)
+            {
+                
+            }
+        }
+    }
+}
